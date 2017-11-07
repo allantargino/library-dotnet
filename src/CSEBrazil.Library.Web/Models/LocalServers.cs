7954329ad7fbc1d917ae9c499408adc49f5f245c
Allan Targino <altargin@microsoft.com>
2017-11-07 17:19:05 -0200

    finishing local testing framework; fixes #6

---
+++ src/CSEBrazil.Library.Web/Models/LocalServers.cs
@@ -0,0 +1,93 @@
+﻿using CSEBrazil.Library.Web.Utils;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+
+namespace CSEBrazil.Library.Web.Models
+{
+    public class LocalServers
+    {
+        private string JsonFileSettings { get; }
+
+        private List<int> Ports { get; }
+        private Dictionary<string, string> ExtraSettings { get; }
+        private Dictionary<string, IWebHostBuilder> Servers { get; }
+
+        private PortManager PortManager { get; }
+
+        public LocalServers(string jsonFileSettings = "appsettings.json")
+        {
+            if (string.IsNullOrEmpty(nameof(jsonFileSettings))) throw new ArgumentNullException(nameof(jsonFileSettings));
+
+            JsonFileSettings = jsonFileSettings;
+
+            ExtraSettings = new Dictionary<string, string>();
+            Servers = new Dictionary<string, IWebHostBuilder>();
+            Ports = new List<int>();
+
+            PortManager = new PortManager();
+        }
+
+        public void AddServer<TStartup>(string serviceName) where TStartup : class
+        {
+            if (string.IsNullOrEmpty(nameof(serviceName))) throw new ArgumentNullException(nameof(serviceName));
+
+            var port = PortManager.GetAvailablePort();
+            Ports.Add(port);
+
+            var serviceHost = GetWebHost<TStartup>(port);
+            Servers.Add(serviceName, serviceHost);
+
+            ExtraSettings.Add($"Services:{serviceName}:Endpoint", $"http://localhost:{port}");
+        }
+
+        public void Start()
+        {
+            IConfigurationRoot configuration = GetConfiguration();
+
+            foreach (var entry in Servers)
+                entry.Value
+                    .UseConfiguration(configuration)
+                    .Build()
+                    .Start();
+        }
+
+        public HttpClient GetClient(string serviceName, string prefix = "api/")
+        {
+            if (string.IsNullOrEmpty(nameof(serviceName))) throw new ArgumentNullException(nameof(serviceName));
+
+            string[] keys = new string[Servers.Keys.Count];
+            Servers.Keys.CopyTo(keys, 0);
+            var index = Array.IndexOf(keys, serviceName);
+
+            return GetClient(Ports[index], prefix);
+        }
+
+        private HttpClient GetClient(int port, string prefix)
+        {
+            return new HttpClient
+            {
+                BaseAddress = new Uri($"http://localhost:{port}/{prefix}")
+            };
+        }
+
+        private IConfigurationRoot GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                        .AddJsonFile(JsonFileSettings)
+                        .AddInMemoryCollection(ExtraSettings)
+                        .Build();
+        }
+
+        private IWebHostBuilder GetWebHost<TStartup>(int port) where TStartup : class
+        {
+            return WebHost.CreateDefaultBuilder()
+                    .UseStartup<TStartup>()
+                    .UseUrls($"http://0.0.0.0:{port}");
+        }
+    }
+}
---
+++ src/CSEBrazil.Library.Web/Utils/PortManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Linq;
+
+namespace CSEBrazil.Library.Web.Utils
+{
+    public class PortManager
+    {
+        public int MaxTentatives { get; }
+        public Random Random { get; }
+
+        public PortManager(int maxTentatives = 10)
+        {
+            MaxTentatives = maxTentatives;
+            Random = new Random();
+        }
+
+        public int GetAvailablePort()
+        {
+            var tentative = 0;
+
+            do
+            {
+                var selectedPort = Random.Next(1024, 65536);
+
+                var properties = IPGlobalProperties.GetIPGlobalProperties();
+                var ports = properties.GetActiveTcpConnections();
+
+                if (ports.Where(p => p.LocalEndPoint.Port == selectedPort).Count() == 0)
+                    return selectedPort;
+                else
+                    tentative++;
+
+            } while (tentative < MaxTentatives);
+
+            throw new InvalidOperationException("Could not assigned a port!");
+        }
+    }
+}
